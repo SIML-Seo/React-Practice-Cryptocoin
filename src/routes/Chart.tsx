21320ece8ceaa6cf80263b93d831b28cd1a3af45
Wonho Seo <swh1182@neolab.net>
2022-03-02 22:19:21 +0900

    initial commit

---
+++ src/routes/Chart.tsx
@@ -0,0 +1,60 @@
+import { useQuery } from "react-query";
+import { useOutletContext } from "react-router-dom";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface ChartProps{
+    coinId: string;
+}
+
+interface IHistorical{
+    time_open: string; 
+    time_close: string; 
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    volume: number;
+    market_cap: number;
+}
+
+function Chart() {
+    const {coinId} = useOutletContext<ChartProps>();
+    const {isLoading, data} = useQuery<IHistorical[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId), {refetchInterval: 10000,});
+    return (
+        <div>
+            {isLoading ? 
+                "Loading Chart..." :
+                <ApexChart 
+                    type="candlestick"
+                    series={[
+                        {
+                            data: data?.map(price => ({x: new Date(price.time_open), y: [price.open, price.high, price.low, price.close]}))
+                        }
+                        // {
+                        //     name: "Price",
+                        //     data: data?.map(price => price.close)
+                        // }
+                    ]}
+                    options={{ 
+                        // chart: {height: 500, width: 500, toolbar: {show: false}, background: "transparent"},
+                        // grid: {show: false},
+                        theme: {mode: "dark"},
+                        // stroke: {curve: "smooth", width: 3 ,},
+                        // xaxis: {labels: {show:false}, axisTicks: {show: false}, axisBorder: {show: false}, categories: data?.map((price) => price.time_close), type: "datetime"},
+                        // yaxis: {show: false},
+                        // fill: {type: "gradient", gradient:{gradientToColors: ["#0be881"], stops: [0, 100]}},
+                        // colors: ["#0fbcf9"],
+                        // tooltip: {y: {formatter: (value) => `$ ${value.toFixed(3)}`}},
+                        chart: {height: 500, type:"candlestick"},
+                        title: {text: "CandleStick Chart", align: "left"},
+                        xaxis: {type: "datetime"},
+                        yaxis: {show: false, tooltip: {enabled: true}},
+                    }}
+                />
+            }
+        </div>    
+    )
+}
+
+export default Chart;
